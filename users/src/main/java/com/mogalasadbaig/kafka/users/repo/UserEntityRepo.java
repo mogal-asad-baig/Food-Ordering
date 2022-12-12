8782c7c5344c1cc5af1834e83503afa7a7eaafc8
MOGAL ASAD BAIG <86158261+mogal-asad-baig@users.noreply.github.com>
2022-12-12 17:38:28 +0530

    Delete users directory

--- users/src/main/java/com/mogalasadbaig/kafka/users/repo/UserEntityRepo.java
+++
@@ -1,9 +0,0 @@
-package com.mogalasadbaig.kafka.users.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.mogalasadbaig.kafka.users.entity.UserEntity;
-
-public interface UserEntityRepo extends MongoRepository<UserEntity, Long>{
-
-}
